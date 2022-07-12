865baed0ceedfac487ca5530c6b2efb9cf02dc26
kclm40 <105953716+kclm40@users.noreply.github.com>
2022-07-11 23:22:58 -0400

    still need commentary

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,14 +6,13 @@ import os
 import csv
 #add budget_data.csv file
 csvpath = os.path.join("Resources","election_data.csv")
-    #correct file path
-    # csvpath = os.path.join('..', 'Resources', 'election_data.csv')
+PyPoll_output = os.path.join("PyPoll.txt")
 
 #define variables
 total_votes = 0
-votes_won = 0
-candidates = {}
-winner = []
+candidate_votes = []
+candidates_list = {}
+winner = 0
 #open the file in "read" mode ('r') and store the contents in the variable "text"
 with open(csvpath) as csvfile:
     #This stores a reference to a file stream
@@ -23,26 +22,51 @@ with open(csvpath) as csvfile:
     # Read each row of data after the header
     
     for row in csvreader:
-        #Total number of votes cast
-        
-        if row[2] not in candidates:
-            candidates[row[2]] = 1
-        else:
-            candidates[row[2]] += 1
-        
-        total_votes += 1
 
-        if votes_won < total_votes:
-            winner = candidates
-    
-    #find candidate names:
-    
-    
-   
+        #Total number of votes cast
+        total_votes += 1 
+
+        candidate = row[2]
+        if candidate in candidates_list:
+            candidates_list[candidate] += 1
+        else:
+            candidates_list[candidate] = 1
 
 print("Election Results")
+
 print('-'*50)
+
 print(f"Total Votes: {total_votes}")
+
 print('-'*50)
-print(f"{candidates}")
-print(f"{winner}")
+            
+for candidate in candidates_list:
+                    
+    percentage_votes = (candidates_list[candidate]/total_votes)
+        
+    print(f"{candidate}: {percentage_votes:.3%} ({candidates_list[candidate]})")     
+
+    if candidates_list[candidate]>winner:
+        winner = candidates_list[candidate]
+        winner_name = candidate
+
+
+print('-'*50)
+
+print(f"Winner: {winner_name}")
+
+print('-'*50)
+
+#print to text file
+
+
+with open(PyPoll_output,'w') as file:
+    file.write("Election Results\n")
+    file.write("=" *50+"\n")
+    file.write(f"Total Votes: {total_votes}\n")
+    file.write("=" *50+"\n")
+    for candidate in candidates_list:
+        file.write(f"{candidate}: {percentage_votes:.3%} ({candidates_list[candidate]})\n")
+    file.write("=" *50+"\n")
+    file.write(f"Winner: {winner_name}\n")
+    
